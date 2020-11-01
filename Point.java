6f92f6f086195ae981673635d17000b780c45502
Logan <lruzzier20@stuy.edu>
2020-11-01 18:45:01 -0500

    added constructors and sample object

--- Point.java
+++ Point.java
@@ -1,4 +1,13 @@
 public class Point{
   private double x, y;
 
+  public Point(double X, double Y){
+    x=X;
+    y=Y;
+  }
+
+  public Point(Point p){
+    x=p.x;
+    y=p.y;
+  }
 }
--- Tester.java
+++ Tester.java
@@ -1,5 +1,5 @@
 public class Tester{
   public static void main(String[] args){
-  
+    Point x = new Point(0, 0);
   }
 }
