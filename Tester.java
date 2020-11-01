1a8cf6b4afc1c212711872c02c517e793c898f7d
Logan <lruzzier20@stuy.edu>
2020-11-01 18:58:17 -0500

    added equals method

--- Tester.java
+++ Tester.java
@@ -1,9 +1,10 @@
 public class Tester{
   public static void main(String[] args){
     Point x = new Point(0, 0);
-    Point y = new Point(3, 4);
+    Point y = new Point(0, 1);
     System.out.println(x.getX());
     System.out.println(x.getY());
     System.out.println(x.distanceTo(y));
+    System.out.println(x.equals(y));
   }
 }
