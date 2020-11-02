0a1cbd2057504a66484cc879ec6e328de009489e
Logan <lruzzier20@stuy.edu>
2020-11-01 19:08:47 -0500

    added both constructors and test cases

--- Tester.java
+++ Tester.java
@@ -2,9 +2,12 @@ public class Tester{
   public static void main(String[] args){
     Point x = new Point(0, 0);
     Point y = new Point(0, 1);
+    Point z = new Point(1, 1);
     System.out.println(x.getX());
     System.out.println(x.getY());
     System.out.println(x.distanceTo(y));
     System.out.println(x.equals(y));
+    Triangle I = new Triangle(x, y, z);
+    Triangle J = new Triangle(0, 0, 3, 0, 3, 4);
   }
 }
--- Triangle.java
+++ Triangle.java
@@ -1,4 +1,15 @@
 public class Triangle{
   public Point v1, v2, v3;
 
+  public Triangle(Point a, Point b, Point c){
+    v1=a;
+    v2=b;
+    v3=c;
+  }
+
+  public Triangle(double x1, double y1, double x2, double y2, double x3, double y3){
+    Point v1 = new Point(x1, y1);
+    Point v2 = new Point(x2, y2);
+    Point v3 = new Point(x3, y3);
+  }
 }
